1a96eaea7d361a5136a3c1627a200bd3e8e68e89
Nero <federixo_fgm@hotmail.com>
2024-09-12 03:40:08 -0300

    arreglado contact form

--- src/pages/send-email.ts
+++ src/pages/send-email.ts
@@ -7,25 +7,19 @@ export const POST: APIRoute = async ({ request }) => {
 
   const resp = await request.json();
   const { name, email, message } = resp;
-  console.log(name, email, message)
+  console.log("name;",name, "email:",email,"message", message)
 
 
   const { data, error } = await resend.emails.send({
     from: 'fguzzodev.com <onboarding@resend.dev>',
     to: ['federixo_fgm@hotmail.com'],
     subject: 'Nevo mensaje desde fguzzodev.com',
-    html: '<strong>'+name+'</strong>, <strong>'+email+'</strong>, <strong>'+message+'</strong>',
+    html: '<strong>Nombre: </strong>'+name+'<br><strong>Teléfono: </strong>'+email+'<br><strong>Mensaje: </strong>'+message+'',
   });
 
   if (error) {
     console.error({ error });
   }
-
-  console.log({ data });
-
-
-
-
   return new Response(JSON.stringify({
     message: "¡se envió correctamente!"
   })
